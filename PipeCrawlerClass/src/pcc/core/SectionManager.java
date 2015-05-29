f229441db4468d115a4379a35334c8f77baf26b5
Yibo <>
2015-05-29 22:06:36 +0100

    Lockfree Buffer

--- PipeCrawlerClass/src/pcc/core/SectionManager.java
+++ PipeCrawlerClass/src/pcc/core/SectionManager.java
@@ -1,7 +1,25 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * The MIT License
+ *
+ * Copyright 2015 yl9.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 package pcc.core;
 
@@ -13,11 +31,11 @@ import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import ppc.dynamic.BlockAnalyser;
-import ppc.dynamic.BlockAnalysisResult;
-import ppc.dynamic.SectionAnalysisResult;
-import ppc.interfaceclass.Buffer;
-import ppc.interfaceclass.Worker;
+import pcc.dynamic.Analysis.BlockAnalyser;
+import pcc.dynamic.Analysis.BlockAnalysisResult;
+import pcc.dynamic.Analysis.SectionAnalysisResult;
+import pcc.interfaceclass.Buffer;
+import pcc.interfaceclass.Worker;
 
 /**
  *
@@ -138,7 +156,7 @@ public class SectionManager {
             }
         }
     }
-
+ 
     public synchronized SectionAnalysisResult GetSectionAnalyseResult() {
         //calculate everything from analysers
         //reset all analysers
