b1fcb456ce46378ca2ff4cf5bf6a32c510b9f5e0
Yibo <yl9@hw.ac.uk>
2016-03-09 20:36:23 +0000

    just another quick fix 20:36:23

--- PipeCrawler/src/pcc/core/entity/RawAccount.java
+++ PipeCrawler/src/pcc/core/entity/RawAccount.java
@@ -51,7 +51,6 @@ public class RawAccount implements Serializable {
     private static final long serialVersionUID = 7513452215622776147L;
     
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     
     @Column(name = "uid", unique=true)
