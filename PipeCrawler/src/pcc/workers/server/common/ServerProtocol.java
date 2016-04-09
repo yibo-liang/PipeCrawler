c62ff7c8af758f3c7126f71aee58ecaf0c12fd36
Yibo <yl9@hw.ac.uk>
2016-04-09 17:59:40 +0100

    just another quick fix 17:59:39

--- PipeCrawler/src/pcc/workers/server/common/ServerProtocol.java
+++ PipeCrawler/src/pcc/workers/server/common/ServerProtocol.java
@@ -161,7 +161,8 @@ public class ServerProtocol implements ServerConnector.IServerProtocol {
                 .add(Restrictions.gt("id", new Long(progress.getLower())))
                 .add(Restrictions.le("id", new Long(progress.getUpper())))
                 //only get 1/10 account due to time/space limitation
-                .add(Restrictions.sqlRestriction("50>rand()*1000"))
+                //comment out to get all for ground truth data
+                //.add(Restrictions.sqlRestriction("50>rand()*1000"))
                 .list();
         if (items.size() > 0) {
             for (int i = 0; i < items.size(); i++) {
